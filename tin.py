522c3675c73dd93811f68870fe7bb50a401f6417
Sam Koolvisutjit <aimods@users.noreply.github.com>
2016-05-11 23:45:40 +0700

    Verify with TIN Service from rd.go.th

---
+++ tin.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import suds.client
+from sslcontext import create_ssl_context, HTTPSTransport
+from suds.sudsobject import asdict
+
+sslverify = False
+cafile = None
+capath = None
+
+kwargs = {}
+sslContext = create_ssl_context(sslverify, cafile, capath)
+kwargs['transport'] = HTTPSTransport(sslContext)
+# Add more suds client kwargs if needed.
+
+#Verify with TIN Service from rd.go.th
+url = 'https://rdws.rd.go.th/serviceRD3/checktinpinservice.asmx?wsdl'
+client = suds.client.Client(url, **kwargs)
+
+results = client.service.ServiceTIN('anonymous','anonymous','5709990004522')
+
+if results.vIsExist:
+    print "OKAY"
+
